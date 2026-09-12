af80ce2f79cd37ae51a2ef9a6656bdac0ad2dd61
Shiva_123 <shiva@ventureconsultancyservices.com>
2026-09-12 10:13:46 +0530

    added: changes in root file

--- src/main.tsx
+++ src/main.tsx
@@ -2,37 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-import { CssBaseline } from '@mui/material'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2',
-    },
-    background: {
-      default: '#f5f7fb',
-    },
-  },
-  shape: {
-    borderRadius: 10,
-  },
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        root: {
-          padding: 12,
-        },
-      },
-    },
-  },
-})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
       <App />
-    </ThemeProvider>
   </StrictMode>,
 )
