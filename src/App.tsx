650a86700800b78d8b607a711c12246d224e93c2
Shiva_123 <shiva@ventureconsultancyservices.com>
2026-09-12 10:09:48 +0530

    Added: changes in files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { Container, Typography } from "@mui/material";
-import useHabitStore from "./store/store";
-import AddHabitForm from "./components/AddHabitForm";
-import HabitList from "./components/HabitList";
-import PhotosGallery from "./components/PhotosGallery";
-
 const App = () => {
-  const store = useHabitStore();
-  console.log(store);
   return (
-     <Container>
-        <Typography variant="h5" component="h5" align="center" gutterBottom>
-          Habit Tracker
-        </Typography>
-        <AddHabitForm />
-        <HabitList />
-        <PhotosGallery />
-     </Container>
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
   );
 };
 
-export default App; 
+export default App;
